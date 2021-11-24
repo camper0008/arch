e44cc65244791cd84e548718b19cd1af41819748
piet <42157469+camper0008@users.noreply.github.com>
2021-11-24 21:23:58 +0100

    fix

--- scripts/partition.sh
+++ scripts/partition.sh
@@ -1,46 +1,35 @@
-
-$FDISKDISPLAYMATCH="                       \
-\'{                                        \
-    isDisk=gsub(/^Disk \/.*$/, "\n"$0);    \
-    isInfo=gsub(/^(Device|\/).*$/, $0);    \
-    if (isDisk==1) {                       \
-        print $0                           \
-    }                                      \
-    if (isInfo==1) {                       \
-        print $0                           \
-    }                                      \
-}\'                                        \
-"          
-
-echo "Input device to partition (ex. /dev/sda)"
-fdisk -l | awk $FDISKDISPLAYMATCH
+FDISKDISPLAYMATCH="{ isDisk=gsub(/^Disk \/.*$/, "\n"\$0); if (isDisk==1) { print \$0 }; isInfo=gsub(/^(Device|\/).*$/, \$0); if (isInfo==1) { print \"  \",\$0 }; }"          
+echo "Input device to partition (ex. /dev/sda)\n"; echo
+fdisk -l | awk "$FDISKDISPLAYMATCH"
+echo; echo "Device name:"
 read DEVICE
 
-cat <<EOF | fdisk /dev/$DEVICE
-d       # wipe existing partitions
-d       # wipe existing partitions
-d       # wipe existing partitions
-d       # wipe existing partitions
-g       # new partition label
-n       # EFI
-        # pick default option
-        # pick default option
-+550M   # 550M space
-n       # swap
-        # pick default option
-        # pick default option
-+2G     # 2G space
-n       # filesystem
-        # pick default option
-        # pick default option
-        # pick default option
-t       # change type
-1       # 1st partition
-1       # to EFI
-t       # change type
-2       # 2nd partition
-19      # to SWAP
-w       # write new partitions 
+cat <<EOF | fdisk $DEVICE
+d
+d
+d
+d
+g
+n
+
+
++550M
+n
+
+
++2G
+n
+
+
+
+t
+1
+1
+t
+2
+19
+w
+y
 EOF
 
 mkfs.fat -F32 "${DEVICE}1"
