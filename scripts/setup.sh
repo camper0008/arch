cd2b941fad22d609db4d2467d8c5169d7aa79dd5
piet <42157469+camper0008@users.noreply.github.com>
2021-11-24 22:55:49 +0100

    small fixes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,21 +1,25 @@
 
 # keyboard
+clear
 echo "Setting keyboard layout"
 loadkeys dk-latin1
 
 # timezone
+clear
 echo "Setting timezone"
 ln -sf /usr/share/zoneinfo/Europe/Copenhagen /etc/localtime
 hwclock --systohc
 
 # locale
+clear
 echo "Generating locales"
 awk '{ gsub(/#en_DK\.UTF-8 UTF-8/, "en_DK.UTF-8 UTF-8"); print }' /etc/locale.gen > /tmp/locale.gen.tmp
-cat /tmp/locale_replaced.gen > /etc/locale.gen
+cat /tmp/locale.gen.tmp > /etc/locale.gen
 rm /tmp/locale.gen.tmp
 locale-gen
 
 # hostname
+clear
 echo "Setting hostname"
 echo "Enter hostname:"
 read HOSTNAME
@@ -23,22 +27,23 @@ echo "$HOSTNAME" > /etc/hostname
 
 echo "Setting hosts"
 echo "\
-127.0.0.1       localhost\
-::1             localhost\
-127.0.1.1       $HOSTNAME.localdomain   $HOSTNAME" >> /etc/hosts
+127.0.0.1       localhost\n\
+::1             localhost\n\
+127.0.1.1       $HOSTNAME.localdomain   $HOSTNAME" > /etc/hosts
 
 # user
-echo "Setting up user"
-echo "Add root password:"
+clear
+echo "Setting up user permissions"
+echo "Add root password"
 passwd
 echo "Adding user"
 echo "Enter username:"
 read USERNAME
 useradd -m $USERNAME
-echo "Add user password:"
 passwd $USERNAME
 
 # user groups
+clear
 echo "Setting up user group"
 usermod -aG wheel,audio,video,optical,storage $USERNAME
 awk '{ gsub(/^# %wheel ALL=\(ALL\) ALL$/, "%wheel ALL=(ALL) ALL"); print }' /etc/sudoers > /tmp/sudoers.tmp
@@ -46,27 +51,28 @@ cat /tmp/sudoers.tmp > /etc/sudoers
 rm /tmp/sudoers.tmp
 
 # grub
+clear
 echo "Setting up grub"
 pacman -S efibootmgr dosfstools os-prober mtools --noconfirm
-echo "Enter device: (ex. /dev/sda)"
+clear
+echo "Enter device you used for partitioning: (ex. /dev/sda)"
 read DEVICE
 mkdir /boot/EFI
-mount "/dev/${DEVICE}1" /boot/EFI
+mount "${DEVICE}1" /boot/EFI
 grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # install packages
+clear
 echo "Installing frequently used packages"
-pacman -S networkmanager vim base-devel gcc git --nonconfirm
+pacman -S networkmanager vim base-devel gcc git --noconfirm
 
 # systemctl
+clear
 echo "Enabling networkmanager"
 systemctl enable NetworkManager
 
 # exit and unmount
-echo "Finishing up"
-exit
-umount -l /mnt
-
-echo "Rebooting"
-reboot
+clear
+echo "Finished commands, unmounting..."
+echo "To complete setup, run 'exit' followed by 'umount -l /mnt && reboot'"
