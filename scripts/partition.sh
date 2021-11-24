50d5168b4e34371231bf56d0ce423f2d2f1a0737
piet <42157469+camper0008@users.noreply.github.com>
2021-11-24 22:41:39 +0100

    fix partioning

--- scripts/partition.sh
+++ scripts/partition.sh
@@ -6,9 +6,17 @@ read DEVICE
 
 cat <<EOF | fdisk $DEVICE
 d
+
+y
 d
+
+y
 d
+
+y
 d
+
+y
 g
 n
 
@@ -37,7 +45,7 @@ mkswap "${DEVICE}2"
 swapon "${DEVICE}2"
 mkfs.ext4 "${DEVICE}3"
 
-mount "/dev/${DEVICE}3" /mnt # mount partition 3 > /mnt
+mount "${DEVICE}3" /mnt # mount partition 3 > /mnt
 pacstrap /mnt base linux linux-firmware # install core into /mnt
 genfstab -U /mnt >> /mnt/etc/fstab # generate file system table
 arch-chroot /mnt
