a18116e9b215ad03e0510430686a20ed0ff5976d
piet <42157469+camper0008@users.noreply.github.com>
2021-11-26 11:11:01 +0000

    small fixes

--- install/scripts/awesome-barebones.sh
+++ install/scripts/awesome-barebones.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
 echo "What video driver do you want to use? (https://wiki.archlinux.org/title/Xorg#Driver_installation)"
-echo "Supported video drivers: \
-xf86-video-amdgpu\
-xf86-video-ati\
-xf86-video-intel\
-xf86-video-nouveau\
-"
+echo "Supported video drivers:"
+echo "xf86-video-amdgpu"
+echo "xf86-video-ati"
+echo "xf86-video-intel"
+echo "xf86-video-nouveau"
 read VIDEO_DRIVER
 
 echo "Installing required packages"
-sudo pacman -S base-devel $VIDEO_DRIVER awesome mesa lib32-mesa xorg xorg-xinit nitrogen picom nerd-fonts-mononoki --no-confirm
+sudo pacman -S base-devel $VIDEO_DRIVER awesome mesa lib32-mesa xorg xorg-xinit nitrogen picom nerd-fonts-mononoki --noconfirm
 
 echo "Configuring"
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
