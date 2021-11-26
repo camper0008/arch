3039416e71b7f6f3cc17dc305f028a30485b8847
piet <42157469+camper0008@users.noreply.github.com>
2021-11-26 11:43:49 +0000

    fix #3

--- install/scripts/awesome-barebones.sh
+++ install/scripts/awesome-barebones.sh
@@ -10,9 +10,11 @@ echo "xf86-video-fbdev (virtualbox)"
 read VIDEO_DRIVER
 
 echo "Installing required packages"
+# split package installations incase it's unable to find one of them
 sudo pacman -S base-devel --noconfirm
 sudo pacman -S $VIDEO_DRIVER --noconfirm
-sudo pacman -S awesome mesa xorg xorg-xinit nerd-fonts-mononoki lib32-mesa --noconfirm
+sudo pacman -S awesome mesa xorg xorg-xinit --noconfirm
+sudo pacman -S nerd-fonts-mononoki lib32-mesa --noconfirm
 
 echo "Configuring"
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
