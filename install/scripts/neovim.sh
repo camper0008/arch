7c6b9372e541488d51d9ceec35681d816d91c647
piet <42157469+camper0008@users.noreply.github.com>
2021-12-07 14:27:45 +0100

    descriptions

--- install/scripts/neovim.sh
+++ install/scripts/neovim.sh
@@ -1,8 +1,10 @@
-echo "Configuring neovim"
+echo "Downloading neovim"
 sudo pacman -S neovim --noconfirm
-mkdir -p ~/.config/nvim/ && cd ~/.config/nvim
-curl -fLo ~/.config/nvim/init.vim --create-dirs \
-    https://raw.githubusercontent.com/camper0008/arch/stable/install/config/.neovim-init.vim
 
+echo "Downloading init.vim"
+curl -fLo ~/.config/nvim/init.vim --create-dirs \
+    https://raw.githubusercontent.com/camper0008/arch/stable/install/config/neovim-init.vim
+
+echo "Downloading vim-plug"
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
