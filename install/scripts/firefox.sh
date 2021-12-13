e3c0a1ffb3a172d30103deecfee6e13518d327d3
piet <42157469+camper0008@users.noreply.github.com>
2021-12-13 11:10:39 +0100

    curl => wget

--- install/scripts/firefox.sh
+++ install/scripts/firefox.sh
@@ -2,8 +2,8 @@ echo "Installing firefox"
 sudo pacman -S firefox --noconfirm
 echo "Downloading configuration"
 echo "Downloading firefox profile"
-curl -fLo ~/.mozilla/firefox/firefox-profile.tar --create-dirs \
-    https://raw.githubusercontent.com/camper0008/arch/main/install/config/firefox-profile.tar
+mkdir ~/.mozilla/firefox/firefox-profile.tar -p
+wget -P ~/.mozilla/firefox/ https://raw.githubusercontent.com/camper0008/arch/main/install/config/firefox-profile.tar
 echo "Unzipping folder"
 echo "Installing dependencies"
 sudo pacman -S tar --noconfirm --needed
@@ -13,6 +13,5 @@ tar -xf firefox-profile.tar
 mv firefox-profile custom.camper
 rm firefox-profile.tar
 echo "Downloading firefox profile settings"
-curl -fLo ~/.mozilla/firefox/profiles.ini --create-dirs \
-    https://raw.githubusercontent.com/camper0008/arch/main/install/config/firefox-profiles.ini
+wget -O profiles.ini -P ~/.mozilla/firefox/ https://raw.githubusercontent.com/camper0008/arch/main/install/config/firefox-profiles.ini
 echo "Setup finished"
