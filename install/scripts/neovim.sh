517c727d8e9b8a5937d03472f30614abf0b0c820
piet <42157469+camper0008@users.noreply.github.com>
2021-12-15 14:04:03 +0100

    Update neovim.sh

--- install/scripts/neovim.sh
+++ install/scripts/neovim.sh
@@ -4,7 +4,11 @@ sudo pacman -S neovim xclip --noconfirm
 mkdir ~/.config/nvim/autoload -p
 
 echo "Downloading init.vim"
-wget -O init.vim -P ~/.config/nvim/ https://raw.githubusercontent.com/camper0008/arch/stable/install/config/neovim-init.vim
+cd ~/.config/nvim/
+wget -O init.vim https://raw.githubusercontent.com/camper0008/arch/stable/install/config/neovim-init.vim
 
 echo "Downloading plug.plug"
-wget -O plug.vim -P ~/.config/nvim/autoload/ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cd ~/.config/nvim/autoload/
+wget -O plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cd 
