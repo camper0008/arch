821711f99075c1308f27941b3b80c67d217aed91
piet <42157469+camper0008@users.noreply.github.com>
2022-03-30 15:10:36 +0200

    Update prompt-shortener.sh

--- install/scripts/prompt-shortener.sh
+++ install/scripts/prompt-shortener.sh
@@ -1,7 +1,7 @@
 sudo pacman -S rustup
 mkdir ~/temp
 cd ~/temp
-git clone https://github.com/camper0008/prompt-shortener-rust
-cd prompt-shortener-rust
+git clone https://github.com/camper0008/prompt-shortener
+cd prompt-shortener
 cargo build --release
 sudo mv $(find target/ -name "prompt-shortener") /usr/local/bin
