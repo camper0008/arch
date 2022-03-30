ceea318e2ebec0224c2bee772eb3650ac3bb1eeb
piet <42157469+camper0008@users.noreply.github.com>
2022-03-30 19:57:00 +0200

    toolchain fixes

--- install/scripts/prompt-shortener.sh
+++ install/scripts/prompt-shortener.sh
@@ -2,9 +2,7 @@ mkdir ~/temp
 cd ~/temp
 git clone https://github.com/camper0008/prompt-shortener
 cd prompt-shortener
-sudo pacman -S rustup
-old_toolchain = $(rustup default)
-rustup toolchain default
+sudo pacman -S rustup --noconfirm
+rustup toolchain install stable
 cargo build --release
 sudo mv $(find target/ -name "prompt-shortener") /usr/local/bin
-echo "note: rustup toolchain is now set to default, was $old_toolchain"
